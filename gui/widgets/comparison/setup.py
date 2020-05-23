25dd5465f8985a62eeaa1e06eb9655b3cff0eff3
Ivan Cheltsov <chip.pragma@gmail.com>
2020-05-23 15:58:22 +0300

    Цвет текста для невыбранного текста

--- gui/widgets/comparison/setup.py
+++ gui/widgets/comparison/setup.py
@@ -2,6 +2,7 @@ import typing as t
 import pathlib as pl
 
 import PySide2.QtCore as qc
+import PySide2.QtGui as qg
 import PySide2.QtWidgets as qw
 from PySide2.QtCore import Qt as qq
 
@@ -22,8 +23,19 @@ class TextFilesProxyModel(qc.QIdentityProxyModel):
 
         if qq.DisplayRole == role:
             return pl.Path(text_file).name
+
         elif qq.CheckStateRole == role:
             return None
+
+        elif qq.ForegroundRole == role:
+            color: qg.QColor = super().data(proxy_index, qq.ForegroundRole)
+            checked: qq.CheckState = super().data(proxy_index, qq.CheckStateRole)
+            if not color:
+                color = qw.QApplication.palette().color(qg.QPalette.Foreground)
+            if qq.Unchecked == checked:
+                color.setAlphaF(0.4)
+            return color
+
         else:
             return super().data(proxy_index, role)
 
