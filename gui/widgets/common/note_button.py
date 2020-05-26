52ab12f0d512b2913cf001c3ad3981930e9fb9b8
Ivan Cheltsov <chip.pragma@gmail.com>
2020-05-26 20:31:05 +0300

    Анализ тектса в отдельной панели с общей кнопкой выполнения
    + Базовый класс настройки анализа
    + Панель настроек анализов
    + Сервер уведомлений
    
    * Кнопка уведомлений работает с сервером

--- gui/widgets/common/note_button.py
+++ gui/widgets/common/note_button.py
@@ -3,32 +3,37 @@ import typing as t
 import PySide2.QtGui as qg
 import PySide2.QtWidgets as qw
 
+from gui.widgets.notification_server import NotificationServer
+
 # language=CSS
 _STYLE_SHEET = """
 QPushButton { 
-    color: rgb(210,105,30); 
     text-align: left;
+    border: 1px solid #FFD800;
+    padding: 2px;
+    background-color: #30FFD800;
+}
+
+QPushButton:hover { 
+    background-color: #FFE97F;
 }
 """
 
 
 class NoteButton(qw.QPushButton):
-    def __init__(self, hide: bool = True, text: str = '', parent: t.Optional[qw.QWidget] = None):
-        super().__init__(qg.QIcon(), text, parent)
+    def __init__(self, server: NotificationServer, parent: t.Optional[qw.QWidget] = None):
+        pixmap = qw.QApplication.style().standardPixmap(qw.QStyle.SP_MessageBoxWarning)
+        super().__init__(qg.QIcon(pixmap), '', parent)
 
         self.clicked.connect(self._on_click)
+        server.notified.connect(self._on_notified)
+        server.cleared.connect(self._on_click)
 
         self.setFlat(True)
         self.setStyleSheet(_STYLE_SHEET)
+        self.hide()
 
-        pixmap = qw.QApplication.style().standardPixmap(qw.QStyle.SP_MessageBoxWarning)
-        self.warning_icon = qg.QIcon(pixmap)
-
-        if hide:
-            self.hide()
-
-    def show_warn(self, text: str):
-        self.setIcon(self.warning_icon)
+    def _on_notified(self, text: str):
         self.setText(text)
         self.show()
 
